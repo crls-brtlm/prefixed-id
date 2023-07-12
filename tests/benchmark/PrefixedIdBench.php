f93135264d1656ee742b3c29b9303217e2dc3f50
Carlos Bartolome <carlos.bartolome@live.com>
2023-07-12 19:57:21 +0200

    added copyright comments

--- tests/benchmark/PrefixedIdBench.php
+++ tests/benchmark/PrefixedIdBench.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the crls-brtlm/prefixed-id library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+declare(strict_types=1);
+
 namespace CrlsBrtlm\PrefixedId\Tests\benchmark;
 
 use CrlsBrtlm\PrefixedId\PrefixedId;
